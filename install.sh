24fee2a144ad054e847be89666241f44621651ce
wass80 <wass80@users.noreply.github.com>
2016-05-23 16:48:07 +0900

    renew install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ cd ~
 git clone https://github.com/wass80/dotfiles
 cd dotfiles
 ln -s .vimrc ~/.vimrc
+ln -s .zshenv ~/.zshenv
 ln -s .zshrc ~/.zshrc
 ln -s .tmux.conf ~/.tmux.conf
 ln -s .gitconfig ~/.gitconfig
